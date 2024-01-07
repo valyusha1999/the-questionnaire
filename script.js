47b7caf26070fb5922b193c77faa8793d72733a3
valyusha1999 <valyusha1999@mail.ru>
2024-01-07 20:55:05 +0300

    new

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 const form = document.querySelector(".form");
+const nameUser = document.querySelector("#name");
+const secondNameUser = document.querySelector("#secondName");
+const emailUser = document.querySelector("#email");
+const phoneUser = document.querySelector("#phone");
+const agreeUser = document.querySelector("#agree");
+
+const userData = {};
+userData.name = nameUser.value;
+userData.secondName = secondNameUser.value;
+userData.phone = phoneUser.value;
+userData.email = emailUser.value;
+userData.agree = true;
+
+
 form.addEventListener("submit", (event) => {
   // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
   // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
+  if (agreeUser.checked === true) {
+    fetch(`https://polinashneider.space/user`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer: valyusha1999'
+      },
+      body: JSON.stringify(userData),
+    })
+    .then((result) => {
+      return result.json()
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err)
+        alert("ОШИБКА!!! Проверьте корректность введённых данных!")
+      });
+  } else {
+    alert("Для отправки анкеты нужно согласиться с обработкой персональных данных");
+  };
 
   // Здесь твой код
 });
